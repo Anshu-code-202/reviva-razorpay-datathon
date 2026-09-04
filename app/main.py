c51a5507f277fdd0a5fffeb2000a8b28ebf61ff0
Anshu <anshuar202@gmail.com>
2026-09-04 21:54:58 +0530

    fix: allow Vercel frontend origin

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://reviva-razorpay-datathon.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
